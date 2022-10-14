305875872efb24bb0d19e96b5101b1e980e7242c
Sahatsawat Sukmak <s6301072510282@email.kmutnb.ac.th>
2022-10-14 21:54:12 +0700

    html css send information

--- test.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    fetch('https://api.exchangerate.host/lastest?base=USD')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
-});
